2003778f5848526b8fd7bb10a783e9e20cd9d673
vladislav <zbrodov10@mail.ru>
2024-12-10 19:06:56 +0300

    Изменил графики: название осей и шкалы. Поменял степень аппроксимации.

--- computation_test.py
+++ computation_test.py
@@ -2,85 +2,77 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-def time_list_multiply(shape, trials=5):
+
+def measure_time(operation, size, dimensions, iterations=30):
     times = []
-    for _ in range(trials):
-        list_a = list(np.random.rand(*shape))
-        list_b = list(np.random.rand(*shape))
+    for _ in range(iterations):
+        if dimensions == 1:
+            a = np.random.rand(size)
+            b = np.random.rand(size)
+        elif dimensions == 2:
+            a = np.random.rand(size, size)
+            b = np.random.rand(size, size)
+        elif dimensions == 3:
+            a = np.random.rand(size, size, size)
+            b = np.random.rand(size, size, size)
+
         start_time = time.time()
-        result = [a * b for a, b in zip(list_a, list_b)]
-        end_time = time.time()
-        times.append(end_time - start_time)
+        operation(a, b)
+        times.append(time.time() - start_time)
+
     return np.mean(times), np.std(times)
 
-def time_numpy_multiply(shape, trials=5):
-    times = []
-    for _ in range(trials):
-        arr_a = np.random.rand(*shape)
-        arr_b = np.random.rand(*shape)
-        start_time = time.time()
-        result = arr_a * arr_b
-        end_time = time.time()
-        times.append(end_time - start_time)
-    return np.mean(times), np.std(times)
 
-sizes = [10, 100, 1000, 5000]
-shapes = [
-    (size,) for size in sizes  # одномерные
-] + [
-    (size, size) for size in sizes  # двумерные
-] + [
-    (size, size, size) for size in sizes  # трехмерные
-]
+sizes = range(10,200,10)
+dimensions = [1, 2, 3]
 
-list_times_mean = []
-list_times_std = []
-for shape in shapes:
-    mean, std = time_list_multiply(shape)
-    list_times_mean.append(mean)
-    list_times_std.append(std)
+list_times = {dim: [] for dim in dimensions}
+list_errors = {dim: [] for dim in dimensions}
+array_times = {dim: [] for dim in dimensions}
+array_errors = {dim: [] for dim in dimensions}
 
-numpy_times_mean = []
-numpy_times_std = []
-for shape in shapes:
-    mean, std = time_numpy_multiply(shape)
-    numpy_times_mean.append(mean)
-    numpy_times_std.append(std)
+for dim in dimensions:
+    for size in sizes:
+        mean_time, std_time = measure_time(lambda a, b: [a[i]*b[i] for i in range(len(a))], size, dim)
+        list_times[dim].append(mean_time)
+        list_errors[dim].append(std_time)
+
+for dim in dimensions:
+    for size in sizes:
+        mean_time, std_time = measure_time(np.multiply, size, dim)
+        array_times[dim].append(mean_time)
+        array_errors[dim].append(std_time)
 
 
-from numpy.polynomial.polynomial import Polynomial
-
-def fit_polynomial(x, y, degree=3):
-    p = Polynomial.fit(x, y, degree)
-    return p
-
-x = np.array(sizes * 3)  # Точки для оси x (размеры)
-y_list = np.array(list_times_mean)  # Время для списков
-y_numpy = np.array(numpy_times_mean)  # Время для numpy массивов
-
-p_list = fit_polynomial(x, y_list)
-p_numpy = fit_polynomial(x, y_numpy)
-
-# Построение графиков
 plt.figure(figsize=(10, 12))
 
-# График 1: Время для списков
 plt.subplot(2, 1, 1)
-plt.errorbar(sizes * 3, list_times_mean, yerr=list_times_std, fmt='o', label='Списки')
-plt.plot(sizes * 3, p_list(x), label='Аппроксимация (списки)', linestyle='--')
-plt.xlabel('Размер (в числе элементов)')
-plt.ylabel('Время (сек)')
-plt.legend()
-plt.title('Время перемножения двух списков')
+for dim in dimensions:
+    plt.errorbar(sizes, list_times[dim], yerr=list_errors[dim], label=f'Lists - Dim {dim}', fmt='o')
+    coeffs = np.polyfit(sizes, list_times[dim], deg=3)
+    poly_appr = np.poly1d(coeffs)
+    plt.plot(sizes, poly_appr(sizes), linestyle='--')
 
-# График 2: Время для numpy массивов
-plt.subplot(2, 1, 2)
-plt.errorbar(sizes * 3, numpy_times_mean, yerr=numpy_times_std, fmt='o', label='Numpy массивы')
-plt.plot(sizes * 3, p_numpy(x), label='Аппроксимация (numpy)', linestyle='--')
-plt.xlabel('Размер (в числе элементов)')
-plt.ylabel('Время (сек)')
+plt.title('Lists')
+plt.xlabel('Size')
+plt.ylabel('Time (seconds)')
+plt.yscale('log')
 plt.legend()
-plt.title('Время перемножения двух numpy массивов')
+plt.grid()
+
+plt.subplot(2, 1, 2)
+for dim in dimensions:
+    plt.errorbar(sizes, array_times[dim], yerr=array_errors[dim], label=f'Numpy Arrays - Dim {dim}', fmt='o')
+    coeffs = np.polyfit(sizes, array_times[dim], deg=3)
+    poly_appr = np.poly1d(coeffs)
+    plt.plot(sizes, poly_appr(sizes), linestyle='--')
+
+plt.title('Numpy Arrays')
+plt.xlabel('Size')
+plt.ylabel('Time (seconds)')
+plt.yscale('log')
+plt.legend()
+plt.grid()
 
 plt.tight_layout()
 plt.savefig('vector_calculation_speed.png')
